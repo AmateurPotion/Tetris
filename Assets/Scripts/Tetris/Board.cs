10107bce4b4f4aeb40494fcfb72d2ae619be9d40
AmteurPotion <1k0j2i4@gmail.com>
2023-03-25 14:34:51 +0900

    Update Board.cs

--- Assets/Scripts/Tetris/Board.cs
+++ Assets/Scripts/Tetris/Board.cs
@@ -1,6 +1,8 @@
-﻿using Tetris.Utils.Attributes;
+﻿using System;
+using Tetris.Utils;
+using Tetris.Utils.Attributes;
+using Tetris.Utils.Extensions;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
 
 namespace Tetris.Tetris
@@ -8,12 +10,20 @@ namespace Tetris.Tetris
     public class Board : MonoBehaviour
     {
         [SerializeField] private Tilemap boardTilemap;
+        [SerializeField] private Tilemap backgroundTilemap;
         [SerializeField, GetSet("size")] private Vector2Int _size = new (10, 20);
+        
+        [Header("Temporary")]
+        [SerializeField] private RuleTile backgroundTile;
+        
         public Vector2Int size
         {
             get => _size;
             set
             {
+                if (value.x < 0) value.X(1);
+                if (value.y < 0) value.Y(1);
+                
                 _size = value;
                 SetBoardSize();
             }
@@ -21,14 +31,13 @@ namespace Tetris.Tetris
 
         public int extraSize = 4;
 
-        [FormerlySerializedAs("tetromino")] public MinoShape minoShape;
+        private void Awake()
+        {
+            backgroundTilemap.SetTiles(Vector2Int.zero, new Vector2Int(_size.x, _size.y), backgroundTile);
+        }
 
         private void SetBoardSize()
         {
-            if (
-                boardTilemap == null ||
-                (_size.x == 0 || _size.y == 0)
-                ) return;
         }
     }
 }
