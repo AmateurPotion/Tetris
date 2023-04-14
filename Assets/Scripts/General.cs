5adb34c8846253b0011d3bd936031b915d8a0d6b
AmteurPotion <1k0j2i4@gmail.com>
2023-04-14 13:48:25 +0900

    arr, das, sdf 설정 추가

--- Assets/Scripts/General.cs
+++ Assets/Scripts/General.cs
@@ -1,12 +1,24 @@
 using Tetris.Game;
+using UnityEngine;
 
 namespace Tetris
 {
     public static class General
     {
+        // Settings
+        // staticMode | false(default) - frame time / true - ms time 
+        public static (int frame, float ms, bool staticMode) ARR, DAS, SDF;
+
         public static void Init()
         {
             MinoShape.Init();
+
+            ARR = (PlayerPrefs.GetInt("arr_i", 1), PlayerPrefs.GetFloat("arr_f", 1),
+                PlayerPrefs.GetInt("arr_m", 0) == 1);
+            DAS = (PlayerPrefs.GetInt("das_i", 1), PlayerPrefs.GetFloat("das_f", 1),
+                PlayerPrefs.GetInt("das_m", 0) == 1);
+            SDF = (PlayerPrefs.GetInt("sdf_i", 1), PlayerPrefs.GetFloat("sdf_f", 1),
+                PlayerPrefs.GetInt("sdf_m", 0) == 1);
         }
     }
 }
