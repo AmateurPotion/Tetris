c3114b1f41e6042399f34dc3821a8c19beed33b1
AmteurPotion <1k0j2i4@gmail.com>
2023-04-24 14:46:43 +0900

    게임 로딩 스크립트 생성

---
+++ Assets/Scripts/Scenes/Init/GameLoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tetris.Scenes.Init
+{
+    public class GameLoader : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+    }
+}
